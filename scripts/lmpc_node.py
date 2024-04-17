364e162debd0b032c5b07e0c90da2d69e691a14f
Siyuan Huang <goamateur@outlook.com>
2024-04-17 12:38:54 -0400

    finish dyna model

--- scripts/lmpc_node.py
+++ scripts/lmpc_node.py
@@ -70,20 +70,95 @@ class LMPC(Node):
         
         dynamics = np.zeros(6)
         h = np.zeros(6)
-        
+        A = np.zeros((nx, nx))
+        B = np.zeros((nx, nu))
         if use_dynamics:
             g = 9.81
-            rear_val = self.car.l_r - accel * self.car.h_cg
-            front_val = self.car.l_f + accel * self.car.h_cg
+            rear_val = g * self.car.l_r - accel * self.car.h_cg
+            front_val = g * self.car.l_f + accel * self.car.h_cg
 
-        
-        self.create_subscription(Odometry, 'ego_racecar/odom', self.pose_callback, 10)
-        self.drive_publisher = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+            dynamics[0] = v * np.cos(yaw+slip_angle)
+            dynamics[1] = v * np.sin(yaw+slip_angle)
+            dynamics[2] = yaw_dot
+            dynamics[3] = accel
+            dynamics[4] = (self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase)) \
+                * (self.car.l_f * self.car.cs_f * steer * rear_val + \
+                    slip_angle * (self.car.l_r * self.car.cs_r * front_val - self.car.l_f * self.car.cs_f * rear_val) \
+                        - yaw_dot/v * (math.pow(self.car.l_f, 2) * self.car.cs_f * rear_val\
+                            + math.pow(self.car.l_r, 2) * self.car.cs_r * front_val))
+            dynamics[5] = (self.car.friction_coeff / (v * (self.car.l_r + self.car.l_f))) \
+                * (self.car.cs_f * steer * rear_val - slip_angle * (self.car.cs_r * front_val + self.car.cs_f * rear_val) \
+                    + (yaw_dot/v) * (self.car.cs_r * self.car.l_r * front_val - self.car.cs_f * self.car.l_f * rear_val)) \
+                        - yaw_dot
+            
+            dfyawdot_dv = (self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase))\
+                * (math.pow(self.car.l_f, 2) * self.car.cs_f * (rear_val) + math.pow(self.car.l_r, 2) * self.car.cs_r * (front_val))\
+                * yaw_dot / math.pow(v, 2)
 
-        self.map_to_car_rotation = None
-        self.map_to_car_translation = None
+            dfyawdot_dyawdot = -(self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase))\
+                            * (math.pow(self.car.l_f, 2) * self.car.cs_f * (rear_val) + math.pow(self.car.l_r, 2) * self.car.cs_r * (front_val))/v
 
-        return A, B, h
+            dfyawdot_dslip = (self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase))\
+                                * (self.car.l_r * self.car.cs_r * (front_val) - self.car.l_f * self.car.cs_f * (rear_val))
+
+            dfslip_dv = -(self.car.friction_coeff / (self.car.l_r + self.car.l_f)) *\
+                        (self.car.cs_f * steer * rear_val - slip_angle * (self.car.cs_r * front_val + self.car.cs_f * rear_val))/math.pow(v,2)\
+                    -2*(self.car.friction_coeff / (self.car.l_r + self.car.l_f)) * (self.car.cs_r * self.car.l_r * front_val - self.car.cs_f * self.car.l_f * rear_val) * yaw_dot/math.pow(v,3)
+
+            dfslip_dyawdot = (self.car.friction_coeff / (math.pow(v,2) * (self.car.l_r + self.car.l_f))) * (self.car.cs_r * self.car.l_r * front_val - self.car.cs_f * self.car.l_f * rear_val) - 1
+
+            dfslip_dslip = -(self.car.friction_coeff / (v * (self.car.l_r + self.car.l_f)))*(self.car.cs_r * front_val + self.car.cs_f * rear_val)
+
+            dfyawdot_da = (self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase))\
+                    *(-self.car.l_f*self.car.cs_f*self.car.h_cg*steer + self.car.l_r*self.car.cs_r*self.car.h_cg*slip_angle + self.car.l_f*self.car.cs_f*self.car.h_cg*slip_angle\
+                    - (yaw_dot/v)*(-math.pow(self.car.l_f,2)*self.car.cs_f*self.car.h_cg) + math.pow(self.car.l_r,2)*self.car.cs_r*self.car.h_cg)
+
+            dfyawdot_dsteer = (self.car.friction_coeff * self.car.mass / (self.car.I_z * self.car.wheelbase)) *\
+                        (self.car.l_f * self.car.cs_f * rear_val)
+
+            dfslip_da = (self.car.friction_coeff / (v * (self.car.l_r + self.car.l_f))) *\
+                    (-self.car.cs_f*self.car.h_cg*steer - (self.car.cs_r*self.car.h_cg - self.car.cs_f*self.car.h_cg)*slip_angle +\
+                    (self.car.cs_r*self.car.h_cg*self.car.l_r + self.car.cs_f*self.car.h_cg*self.car.l_f)*(yaw_dot/v))
+
+            dfslip_dsteer = (self.car.friction_coeff / (v * (self.car.l_r + self.car.l_f))) *\
+                    (self.car.cs_f * rear_val)
+            
+            A[0, 2] = -v * np.sin(yaw+slip_angle)
+            A[0, 3] = np.cos(yaw+slip_angle)
+            A[0, 5] = -v * np.sin(yaw+slip_angle)
+            A[1, 2] = v * np.cos(yaw+slip_angle)
+            A[1, 3] = np.sin(yaw+slip_angle)
+            A[1, 5] = v * np.cos(yaw+slip_angle)
+            A[2, 4] = 1
+            A[4, 3] = dfyawdot_dv
+            A[4, 4] = dfyawdot_dyawdot
+            A[4, 5] = dfyawdot_dslip
+            A[5, 3] = dfslip_dv
+            A[5, 4] = dfslip_dyawdot
+            A[5, 5] = dfslip_dslip
+            
+            B[3, 0] = 1
+            B[4, 0] = dfyawdot_da
+            B[4, 1] = dfyawdot_dsteer
+            B[5, 0] = dfslip_da
+            B[5, 1] = dfslip_dsteer
+        else:
+            dynamics[0] = v * np.cos(yaw)
+            dynamics[1] = v * np.sin(yaw)
+            dynamics[2] = v * np.tan(steer) / self.car.wheelbase
+            dynamics[3] = accel
+            dynamics[4] = 0
+            dynamics[5] = 0
+
+            A[0, 2] = -v * np.sin(yaw)
+            A[0, 3] = np.cos(yaw)
+            A[1, 2] = v * np.cos(yaw)
+            A[1, 3] = np.sin(yaw)
+            A[2, 3] = np.tan(steer) / self.car.wheelbase
+            
+            B[2, 1] = v / (self.car.wheelbase * np.cos(steer)**2)
+            B[3, 0] = 1
+        return Ad, Bd, hd
 
     def pose_callback(self, msg):
         current_pose = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y])
