ce21530905daa435e81748eb46c102107d388e0d
Siyuan Huang <goamateur@outlook.com>
2024-04-17 23:20:07 -0400

    update car params for MPC solve

--- scripts/car_params.py
+++ scripts/car_params.py
@@ -11,4 +11,11 @@ class CarParams:
     cs_r: float
     mass: float
     I_z: float
+    K_NEAR: int
+    N: int
+    VEL_MAX: float
+    ACCELERATION_MAX: float
+    DECELERATION_MAX: float
+    STEER_MAX: float
+    DYNA_VEL_THRESH: float
     
